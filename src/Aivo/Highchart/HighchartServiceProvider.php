3c10c76d8aa4cf5aa8e3fcee915def485bb424fb
Mauro Moreno <moreno.mauro.emanuel@gmail.com>
2015-02-10 09:29:55 -0300

    Documentation

--- src/Aivo/Highchart/HighchartServiceProvider.php
+++ src/Aivo/Highchart/HighchartServiceProvider.php
@@ -1,12 +1,25 @@
 <?php
-
+/**
+ * Highchart Service Provider for Laravel
+ *
+ * @author Mauro Moreno <mmoreno@aivo.co>
+ */
 namespace Aivo\Highchart;
 
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * HighchartServiceProvider extends ServiceProvider
+ *
+ */
 class HighchartServiceProvider extends ServiceProvider
 {
 
+    /**
+     * Register function used for Service Provider in Laravel
+     *
+     * @return Chart
+     */
     public function register()
     {
         $this->app->bind('highchart', function($app) {
